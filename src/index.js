dc58116afaf198796ec501576db4cfa5268ad081
Carlos Diego Ticona Arias - 822144467 <822144467@ulife.com.br>
2023-04-04 21:31:32 -0300

    aula props react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div className="container border rounded mt-2">
+            <div className='row'>
+                <div className='col-12'>
+                    <h1 className='display-5
+                    text-center'>Seus Pedidos</h1>
+                </div>
+            </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+            <div className='row'>
+                <div className="col-12 col-lg-6 col-xl-4">
+                <div className="card">
+                <div className="card-header text-muted">22/04/2022</div>
+                <div className="card-body d-flex">
+                    <div className="d-flex align-items-center">
+                    <div className="fa-solid fa-shake fa-hard-drive fa-2x"></div> 
+                    </div>
+
+                    <div className="flex-grow-1 ms-2 border">
+                        <h4 className="text-center">SSD</h4>
+                         <p className="text-center">SSD Kingston A400 - SATA</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-header text-muted">22/04/2022</div>
+                <div className="card-body d-flex">
+                    <div className="d-flex align-items-center">
+                    <div className="fa-solid fa-shake fa-hard-drive fa-2x"></div> 
+                    </div>
+
+                    <div className="flex-grow-1 ms-2 border">
+                        <h4 className="text-center">SSD</h4>
+                         <p className="text-center">SSD Kingston A400 - SATA</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-header text-muted">22/04/2022</div>
+                <div className="card-body d-flex">
+                    <div className="d-flex align-items-center">
+                    <div className="fa-solid fa-shake fa-hard-drive fa-2x"></div> 
+                    </div>
+
+                    <div className="flex-grow-1 ms-2 border">
+                        <h4 className="text-center">SSD</h4>
+                         <p className="text-center">SSD Kingston A400 - SATA</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-header text-muted">22/04/2022</div>
+                <div className="card-body d-flex">
+                    <div className="d-flex align-items-center">
+                    <div className="fa-solid fa-shake fa-hard-drive fa-2x"></div> 
+                    </div>
+
+                    <div className="flex-grow-1 ms-2 border">
+                        <h4 className="text-center">SSD</h4>
+                         <p className="text-center">SSD Kingston A400 - SATA</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+        
+    )
+}
+ReactDOM.render(
+ <App />,
+ document.querySelector('#root')
+)
